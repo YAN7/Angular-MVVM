69127f8b0e763632cee04dbcbbe136911da70cfc
YAN7 <904394373@qq.com>
2020-04-09 22:13:47 +0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatListModule,
+  MatDividerModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const CommonModules = [
+  FormsModule,
+  ReactiveFormsModule,
+  BrowserAnimationsModule,
+  FlexLayoutModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatListModule,
+  MatDividerModule,
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...CommonModules,
   ],
   providers: [],
   bootstrap: [AppComponent]
